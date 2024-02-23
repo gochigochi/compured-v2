ea1af30bd1a5df8694e2c8c82c21373580a34ccc
diego <diegoeliseoiovane@gmail.com>
2024-02-23 12:12:23 -0300

    initial commit + init store + layout

---
+++ components/ui/layout/nav/Nav.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <header className="relative z-[800] floating-box-shadow">
+        <div className="max-w-screen-2xl mx-auto px-3">
+
+        </div>
+    </header>
+  )
+}
+
+export default Nav
